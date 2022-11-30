b82e74421b193bf4e153e15e994acf09b5e2fc70
Arreme <a.reme@hotmail.com>
2022-11-30 15:52:24 +0100

    Testing settings

--- src/api/include/command.hpp
+++ src/api/include/command.hpp
@@ -21,7 +21,7 @@ public:
         command.add_option
                 (
                     dpp::command_option(dpp::co_string, "animal", "The type of animal", true).
-                        add_choice(dpp::command_option_choice("Dog", std::string("animal_dog"))).
+                        add_choice(dpp::command_option_choice("Horse", std::string("animal_horse"))).
                         add_choice(dpp::command_option_choice("Cat", std::string("animal_cat"))).
                         add_choice(dpp::command_option_choice("Penguin", std::string("animal_penguin")
                 )
