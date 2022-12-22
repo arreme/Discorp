697c58c7fee2d2627d235b655ce55480a31c33c9
Arreme <a.reme@hotmail.com>
2022-12-23 00:31:33 +0100

    Erased legacy code

--- legacy/business/objects/object.cpp
+++
@@ -1,7 +0,0 @@
-#include <object.h>
-
-Object::Object(std::string itemName, bool canSell, int marketValue,std::string resourceName) :
-itemName(itemName),canSell(canSell),marketValue(marketValue) ,resourceName(resourceName)
-{
-    
-};
--- legacy/commands/include/command.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <dpp.h>
-#include <gd.h>
-class Command {
-    public:
-        virtual void CreateCommand(dpp::commandhandler *handler) = 0;
-    protected:
-        dpp::commandhandler *handler;
-};
